d4685f01a1d53af9732ebf6818361a3d76664421
Juan Alvarez <juaniignalvarez@gmail.com>
2025-12-19 20:34:47 -0300

    Enhance LoginForm component with improved styling and structure

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -12,20 +12,30 @@ export const LoginForm = ({ onLogin }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <button type="submit" >Login</button>
-    </form>
+    <>
+      <h4 className="text-center text-2xl font-semibold mt-20">Iniciar Sesión</h4>
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-20 p-6 border rounded-lg shadow-md">
+        <input
+          className="mb-4 w-full px-4 py-2 rounded-lg border focus:outline-none focus:ring-2 focus:ring-primary"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          className="mb-4 w-full px-4 py-2 rounded-lg border focus:outline-none focus:ring-2 focus:ring-primary"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          type="submit"
+          className="w-full px-4 py-2 rounded-lg transition-colors bg-blue-500 hover:bg-blue-600 text-white font-medium"
+        >
+          Login
+        </button>
+      </form>
+    </>
   );
 };
