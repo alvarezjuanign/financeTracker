ff666487689022eee4b083a6f7cf9ecb9d2fd549
Juan Alvarez <juaniignalvarez@gmail.com>
2026-01-20 16:08:13 -0300

    Refactor LoginForm component to enhance layout, add logo, and update text for better user experience

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import LogoFin from "/icons/FinTrackerLogo.svg";
 
 export const LoginForm = ({ onLogin }) => {
   const [email, setEmail] = useState("");
@@ -12,30 +13,39 @@ export const LoginForm = ({ onLogin }) => {
   }
 
   return (
-    <>
-      <h4 className="text-center text-2xl font-semibold mt-20">Iniciar Sesión</h4>
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-20 p-6 border rounded-lg shadow-md">
+    <main className="h-screen flex flex-col justify-center">
+      <img src={LogoFin} alt="Finance Tracker Logo" className="mx-auto mb-4 h-32" />
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-5 p-6 border border-gray-200 rounded-lg shadow-2xl">
+        <h4 className="text-center text-2xl font-semibold mb-6">Bienvenido</h4>
+        <label htmlFor="email" className="mb-2 block">Email</label>
         <input
           className="mb-4 w-full px-4 py-2 rounded-lg border focus:outline-none focus:ring-2 focus:ring-primary"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
+          placeholder="example@example.com"
+          id="email"
+          name="email"
         />
+        <label htmlFor="password" className="mb-2 block">Contraseña</label>
         <input
           className="mb-4 w-full px-4 py-2 rounded-lg border focus:outline-none focus:ring-2 focus:ring-primary"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
+          placeholder="********"
         />
         <button
           type="submit"
-          className="w-full px-4 py-2 rounded-lg transition-colors bg-blue-500 hover:bg-blue-600 text-white font-medium"
+          className="w-full px-4 py-2 rounded-lg transition-colors bg-blue-500 hover:bg-blue-600 text-white font-medium hover:cursor-pointer"
         >
-          Login
+          Iniciar Sesión
+        </button>
+        <hr className="my-6" />
+        <button className="w-full px-4 py-2 rounded-lg transition-colors bg-gray-500 hover:bg-gray-600 text-white font-medium hover:cursor-pointer">
+          Iniciar Sesión como Invitado
         </button>
       </form>
-    </>
+    </main>
   );
 };
